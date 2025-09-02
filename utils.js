1e9f4a842434b6891f47afda3a8e4e6ba1c0a2de
Štěpán Mynařík <46678816+StepanMynarik@users.noreply.github.com>
2025-09-02 12:05:29 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Econea Utils
 // @namespace    https://econea.cz/
-// @version      1.3.35
+// @version      1.3.36
 // @description  Replaces specified Shopify metafield editors with Suneditor WYSIWYG editor etc.
 // @author       Stepan
-// @match        https://*.myshopify.com/admin/products/*
-// @match        https://admin.shopify.com/store/*/products/*
+// @match        https://*.myshopify.com/admin/*
+// @match        https://admin.shopify.com/store/*
 // @require      https://cdn.jsdelivr.net/npm/suneditor@2.47.7/dist/suneditor.min.js
 // @require      https://cdn.jsdelivr.net/npm/suneditor@2.47.7/src/lang/cs.js
 // @resource     SuneditorCSS https://cdn.jsdelivr.net/npm/suneditor@2.47.7/dist/css/suneditor.min.css
